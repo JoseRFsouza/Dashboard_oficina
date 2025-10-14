286274e0b7bcbe10a77968cfaf3acc560d98dad7
JoseRFsouza <joserfsouza@hotmail.com>
2025-10-14 00:55:23 -0300

    ESLint rules adjustment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,22 @@
 import { FlatCompat } from '@eslint/eslintrc'
- 
+
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
 })
- 
+
 const eslintConfig = [
   ...compat.config({
-    extends: ['next', 'next/core-web-vitals','next/typescript'],
+    extends: ['next', 'next/core-web-vitals', 'next/typescript'],
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
+
+      // 👇 Desliga ou relaxa as regras problemáticas
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'prefer-const': 'warn',
     },
   }),
 ]
- 
+
 export default eslintConfig
